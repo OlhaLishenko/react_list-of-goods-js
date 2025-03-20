6b8fda96d58a8f5edbb82e6c4df868f643ae4c66
Olha Lishenko <lishenko.olha@gmail.com>
2025-03-20 16:26:44 +0200

    task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
 
-import { SortButtons } from './SortButtons';
-import { GoodList } from './GoodList';
+// import { SortButtons } from './SortButtons';
+// import { GoodList } from './GoodList';
 
 export const goodsFromServer = [
   'Dumplings',
@@ -17,6 +17,86 @@ export const goodsFromServer = [
   'Jam',
   'Garlic',
 ];
+
+export const SortButtons = ({ visibleGoods, setVisibleGoods, goods }) => {
+  const sortButtons = [
+    'Sort alphabetically',
+    'Sort by length',
+    'Reverse',
+    'Reset',
+  ];
+
+  const INFO = 'is-info';
+  const SUCCESS = 'is-success';
+  const WARNING = 'is-warning';
+  const DANGER = 'is-danger';
+
+  const [activeButton, setActiveButton] = useState('');
+
+  useEffect(() => {
+    if (activeButton === 'Sort alphabetically') {
+      setVisibleGoods(
+        [...visibleGoods].sort((good1, good2) => good1.localeCompare(good2)),
+      );
+    }
+
+    if (activeButton === 'Sort by length') {
+      setVisibleGoods(
+        [...visibleGoods].sort((good1, good2) => good1.length - good2.length),
+      );
+    }
+
+    if (activeButton === 'Reverse') {
+      setVisibleGoods([...visibleGoods].reverse());
+    }
+
+    if (activeButton === 'Reset') {
+      setVisibleGoods(goods);
+    }
+  }, [activeButton]);
+
+  return (
+    <div className="buttons">
+      {sortButtons.map(button => (
+        <button
+          key={button}
+          type="button"
+          className={cn(
+            'button',
+            [
+              button === 'Sort alphabetically' && INFO,
+              button === 'Sort by length' && SUCCESS,
+              button === 'Reverse' && WARNING,
+              button === 'Reset' && DANGER,
+            ],
+            {
+              'is-light': activeButton !== button,
+            },
+          )}
+          onClick={() =>
+            setActiveButton(prev =>
+              // eslint-disable-next-line prettier/prettier
+              prev !== button ? button : setVisibleGoods(goodsFromServer))
+          }
+        >
+          {button}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export const GoodList = ({ goods }) => {
+  return (
+    <ul>
+      {goods.map(good => (
+        <li key={good} data-cy="Good">
+          {good}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export const App = () => {
   const [visibleGoods, setVisibleGoods] = useState(goodsFromServer);
--- src/GoodList.jsx
+++ src/GoodList.jsx
@@ -1,11 +1,1 @@
-export const GoodList = ({ goods }) => {
-  return (
-    <ul>
-      {goods.map(good => (
-        <li key={good} data-cy="Good">
-          {good}
-        </li>
-      ))}
-    </ul>
-  );
-};
+
